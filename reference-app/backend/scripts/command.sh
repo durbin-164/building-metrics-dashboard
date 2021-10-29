53b31a95f62df2f0725a0cf67652f47a9455df9f
Masud Rana <masud.rana@infolytx.com>
2021-10-30 03:01:06 +0600

    update readme and code

--- reference-app/backend/scripts/command.sh
+++ reference-app/backend/scripts/command.sh
@@ -1,2 +1,2 @@
-docker build -t durbin164/building-monitoring-backend:v2 .
-docker push durbin164/building-monitoring-backend:v2
+docker build -t durbin164/building-monitoring-backend:v4 .
+docker push durbin164/building-monitoring-backend:v4
